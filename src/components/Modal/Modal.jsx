c2e97832aa4e34aee28ed92d0e422ef237e27286
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-09-20 14:52:31 +0200

    fix modal close

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,15 +4,16 @@ import PropTypes from 'prop-types';
 export const Modal = ({ largeImageURL, tags, onClick }) => {
   const { image, modal, overlay } = styles;
 
+  const handleOverlayClick = event => {
+    if (event.target === event.currentTarget) {
+      onClick();
+    }
+  };
+
   return (
-    <div className={overlay}>
+    <div className={overlay} onClick={handleOverlayClick}>
       <div className={modal}>
-        <img
-          className={image}
-          src={largeImageURL}
-          alt={tags}
-          onClick={onClick}
-        />
+        <img className={image} src={largeImageURL} alt={tags} />
       </div>
     </div>
   );
